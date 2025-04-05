ac48f3b9e3fccb9af7f9205189d1d88ceace2013
czMikeTan <mike.t@wustl.edu>
2025-04-05 14:34:52 -0500

    basic

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -29,7 +29,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'Gemini',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
